6c6ca208bb75ff44a39616189f570fb526ba5849
janwalec <jasiakpasiak@gmail.com>
2022-11-16 21:54:02 +0100

    liczby pierwsze

---
+++ Liczby Pierwsze.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    
+        int number;
+        int index=0;
+        int how_many;
+        cin >> how_many;
+
+        for (int i = 0; i < how_many; i++)
+        {
+            index = 0;
+            cin >> number;
+            for (int i = number; i > 0; i--)
+            {
+                if (number % i == 0)
+                {
+                    index++;
+                }
+            }
+
+            if (index == 2)
+            {
+                cout << "TAK\n";
+            }
+            if (index != 2)
+            {
+                cout << "NIE\n";
+            }
+        }
+    
+}
+
