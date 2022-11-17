4c293330c84635eab7eca775d54196810b174b5f
janwalec <jasiakpasiak@gmail.com>
2022-11-17 10:41:41 +0100

    dodawanie

---
+++ dodawanie.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+	int how_many;
+	cin >> how_many;
+
+	for (int i = 0; i < how_many; i++)
+	{
+		int quantity;
+		cin >> quantity;
+		int temp = 0;
+		int number;
+		for (int j = 0; j < quantity; j++)
+		{
+			cin >> number;
+			temp += number;
+		}
+		cout << temp<<endl;
+	}
+}
