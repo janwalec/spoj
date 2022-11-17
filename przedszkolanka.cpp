8eaa8144ebaf9b561c52604c401f8625dc3ecf81
janwalec <jasiakpasiak@gmail.com>
2022-11-17 10:41:33 +0100

    przedszkolanka

---
+++ przedszkolanka.cpp
@@ -0,0 +1,31 @@
+﻿	#include <iostream>
+	using namespace std;
+
+	int main()
+	{
+		long int grupa1;
+		long int grupa2;
+		int how_many;
+		cin >> how_many;
+	
+		for (int j = 0; j < how_many; j++)
+		{
+			cin >> grupa1;
+			cin >> grupa2;
+			int iloczyn = grupa1 * grupa2;
+			while (grupa1 != grupa2)
+			{
+				if (grupa1 > grupa2)
+				{
+					grupa1 -= grupa2;
+				}
+				else
+				{
+					grupa2 -= grupa1;
+				}
+			}
+			cout << iloczyn/grupa1<<endl;
+		}
+		return 0;
+	}
+
