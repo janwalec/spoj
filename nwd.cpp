ae6e00ffabe11a7e24052d7abd517e485e7ed884
janwalec <jasiakpasiak@gmail.com>
2022-11-19 13:41:33 +0100

    nwd

---
+++ nwd.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int nwd(int a, int b);
+
+int main()
+{
+	int how_many;
+	cin >> how_many;
+	for (int i = 0; i < how_many; i++)
+	{
+		int first_number;
+		int second_number;
+		cin >> first_number;
+		cin >> second_number;
+		cout << nwd(first_number, second_number)<<endl;
+	}
+}
+
+int nwd(int a, int b)
+{
+	while (a != b)
+	{
+		if (a > b) a -= b;
+		else b -= a;
+	}
+	return a;
+
+}
