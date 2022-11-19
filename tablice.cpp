34424c5beae251f1f0fb7bc97752c6cea1a7e355
janwalec <jasiakpasiak@gmail.com>
2022-11-19 13:41:40 +0100

    tablice

---
+++ tablice.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#define SIZE 100
+using namespace std;
+
+int main()
+{
+    int how_many_operations;
+    cin >> how_many_operations;
+
+    for (int i = 0; i < how_many_operations; i++)
+    {
+
+        int number_of_cells;
+        cin >> number_of_cells;
+
+        int tab[SIZE];
+        for (int i = 0; i < number_of_cells; i++)
+        {
+            cin >> tab[i];
+        }
+
+        for (int i = number_of_cells - 1; i >= 0; i--)
+        {
+            cout << tab[i] << ' ';
+        }
+        cout << endl;
+    }
+
+}
